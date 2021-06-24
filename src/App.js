147c6d861ae922f8d016f6c58f03e0e929b9ca7e
rohithrobin2 <rohithrobin2@gmail.com>
2021-06-24 13:06:20 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, {Component} from 'react';
+import {MenuItem} from './components/menu';
+import "./components/menu.css"
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[]
+    };
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className=" " >
+     
+      <div className="Style" >
+        <div className="Container">
+      <h3 className="color">Menu</h3>
+     
+   <MenuItem name="hell2"/>
+      </div>
+      <div className="Style3">
+      <h3 >Project </h3>
+      <h3>Project name</h3>
+      <h3>Client </h3>
+      <h3>Client name</h3>
+      <h3>Due date </h3>
+      <h3>Status </h3>
+      <h3>Engagement partner</h3>
+      <h3>Team Lead</h3>
+      
+      
+      </div>
+     
+      </div>
+
+      <div className="Body">
+        <h1>hello</h1>
+      </div>
       </header>
+    <body className="Body"> <h1>hello</h1></body>
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/menu.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./menu.css"
+
+    export const MenuItem  = props =>{
+
+    return (
+        <div className="Container" > 
+            
+            <p>Project Specifies </p>
+            <p>Documentation </p>
+            <p>Update due date </p>
+            <p>Add specific activity </p>
+            <p>Book time </p>
+            <p>Update Status </p>
+            <p>Team Members </p>
+            <p>Related projects </p>
+            <p>Project Diary </p>
+            <p>Archive</p>
+            <p>Internal Reference</p>
+            <p>UDIN </p>
+            <p>DSC Sign Register</p>
+            <p>Document In </p>
+            <p>Document Out </p>
+            <p>Project Edit</p>
+            <p>Billing </p>
+
+        </div>
+    )
+}
+
